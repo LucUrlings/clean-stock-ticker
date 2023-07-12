43e84677685fb1071b101df8da38b2e8d644a716
lucurlings <urlingsluc@gmail.com>
2023-07-12 22:49:23 +0200

    Setup colours, cleaned page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "bg-color": "#323437",
+      "main-color": "#e2b714",
+      "sub-color": "#646669",
+      "base": "#000000",
+      "base-light": "#ffffff",
+      "base-red": "#eb0004"
+    },
+    extend: {
+    },
   },
   plugins: [],
 } satisfies Config;
