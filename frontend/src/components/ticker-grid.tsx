c510ade69d93ed9d80290c5947bd1baddf78ea73
Luc Urlings <urlingsluc@gmail.com>
2023-07-25 21:22:03 +0200

    Restarted project

--- frontend/src/components/ticker-grid.tsx
+++
@@ -1,7 +0,0 @@
-export const TickerGrid = () => {
-    return (
-        <div>
-
-        </div>
-    )
-}
--- ws-backend/src/utils/logger.ts
+++
@@ -1,7 +0,0 @@
-type severity = "INFO" | "WARN" | "ERROR"
-
-export const log = (logLevel: severity, ...message: any[]) => {
-    if (logLevel === "INFO") console.log(`INFO   [${new Date().toLocaleString()}] -`, ...message)
-    if (logLevel === "WARN") console.warn(`WARN   [${new Date().toLocaleString()}] -`, ...message)
-    if (logLevel === "ERROR") console.error(`\x1b[31mERROR  [${new Date().toLocaleString()}]\x1b[0m -`, ...message)
-}
