00dc350745adbb3e420089dde2c014152ca1bfb4
Luc Urlings <urlingsluc@gmail.com>
2023-07-29 16:50:21 +0200

    updated router

--- src/server/api/router.ts
+++ src/server/api/router.ts
@@ -1,6 +1,6 @@
 import { EventEmitter } from 'events';
 import {initTRPC, router} from '@trpc/server';
-import {tickerRouter} from "~/server/api/routers/ticker";
+import {tickerRouter} from './routers/ticker';
 
 // create a global event emitter (could be replaced by redis, etc)
 const ee = new EventEmitter();
