9a0d4827601caa45bcf5fb845887f044283ad81d
Luc Urlings <urlingsluc@gmail.com>
2023-07-30 15:54:45 +0200

    trying to fix websockets on server #5

--- src/server/prodServer.ts
+++ src/server/prodServer.ts
@@ -6,7 +6,7 @@ import next from 'next';
 import { parse } from 'url';
 import ws from 'ws';
 
-const port = parseInt(process.env.PORT || '3000', 10);
+const port = parseInt(process.env.PORT ?? '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
